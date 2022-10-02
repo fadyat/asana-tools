4223495611f461b92a378fbeca6c4b136d2a4dbe
fadyat <fadyat@icloud.com>
2022-10-02 13:53:22 +0300

    feat: split asana component to a http client

--- src/clients/asana/component.py
+++ src/clients/asana/component.py
@@ -1,17 +1,15 @@
-import json
-import types
 import typing
 
 import aiohttp
 
 from src import settings
-from src.clients.asana.before_request import remove_none_values
-from src.clients.asana.responses.base import catch_asana_api_error
 
 __all__ = ('AsyncAsanaClientComponent',)
 
+from src.clients.base import HttpClient
 
-class AsyncAsanaClientComponent:
+
+class AsyncAsanaClientComponent(HttpClient):
     def __init__(
         self,
         access_token: str,
@@ -19,114 +17,10 @@ class AsyncAsanaClientComponent:
         http_session: aiohttp.ClientSession | None = None,
         headers: typing.Mapping[str, str] | None = None,
     ):
-        self.http_session = http_session or aiohttp.ClientSession()
-        self.access_token = access_token
-        self.api_endpoint = asana_api_endpoint
-        self._headers = headers or {}
-
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(
-        self,
-        exc_type: typing.Type[BaseException] | None,
-        exc_val: BaseException | None,
-        exc_tb: types.TracebackType,
-    ) -> None:
-        await self.http_session.close()
-
-    @property
-    def headers(self):
-        return {
-            "Authorization": f"Bearer {self.access_token}",
-            **self._headers,
-        }
-
-    async def get(
-        self,
-        endpoint: str,
-        params: typing.Mapping | None = None,
-        body: typing.Mapping | None = None,
-    ):
-        return await self._request(
-            method='get', endpoint=endpoint, params=params, body=body
+        headers = headers or {}
+        headers['Authorization'] = f'Bearer {access_token}'
+        super().__init__(
+            base_endpoint=asana_api_endpoint,
+            http_session=http_session,
+            headers=headers,
         )
-
-    async def post(
-        self,
-        endpoint: str,
-        params: typing.Mapping | None = None,
-        body: typing.Mapping | None = None,
-    ):
-        return await self._request(
-            method='post',
-            endpoint=endpoint,
-            params=params,
-            body=body,
-        )
-
-    async def put(
-        self,
-        endpoint: str,
-        params: typing.Mapping | None = None,
-        body: typing.Mapping | None = None,
-    ):
-        return await self._request(
-            method='put',
-            endpoint=endpoint,
-            params=params,
-            body=body,
-        )
-
-    async def patch(
-        self,
-        endpoint: str,
-        params: typing.Mapping | None = None,
-        body: typing.Mapping | None = None,
-    ):
-        return await self._request(
-            method='patch',
-            endpoint=endpoint,
-            params=params,
-            body=body,
-        )
-
-    async def delete(
-        self,
-        endpoint: str,
-        params: typing.Mapping | None = None,
-        body: typing.Mapping | None = None,
-    ):
-        return await self._request(
-            method='delete',
-            endpoint=endpoint,
-            params=params,
-            body=body,
-        )
-
-    @catch_asana_api_error
-    async def _request(
-        self,
-        method: str,
-        endpoint: str,
-        params: typing.Mapping | None = None,
-        body: typing.Mapping | None = None,
-    ):
-        if not isinstance(body, typing.Mapping):
-            body = {}
-
-        if not isinstance(params, typing.Mapping):
-            params = {}
-
-        params = remove_none_values(params)
-        body = remove_none_values(body)
-
-        response = await self.http_session.request(
-            method=method,
-            url=f"{self.api_endpoint}/{endpoint}",
-            headers=self.headers,
-            params=params,
-            data=json.dumps(body),  # errors sometimes here
-        )
-
-        return await response.json()
