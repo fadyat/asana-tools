49fd43f5787f744845f13a03640278fb643cce73
fadyat <fadyat@icloud.com>
2023-07-09 17:00:31 +0300

    feat: show activity response as a mui table

--- frontend/src/components/activity/activity-response.tsx
+++ frontend/src/components/activity/activity-response.tsx
@@ -1,20 +1,38 @@
 import {ActivitySuccessResponse} from "../../api/asana/activity";
 import "./activity-response.css"
+import {Paper, Table, TableCell, TableContainer, TableHead, TableRow} from "@mui/material";
+import React from "react";
 
 const ActivityResponse = (props: ActivitySuccessResponse) => {
+
     return (
-        <div className="activity-response">
-            <h3 className="activity-response-total"> Total users: {props.length}</h3>
-            {
-                props.result.map((item, index) => {
-                    return (
-                        <div key={index} className="activity-response-item">
-                            {item.user_email}: {item.actions_cnt}
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <TableContainer component={Paper}
+                        sx={{width: '100%', marginTop: '20px'}}
+        >
+            <Table size="small"
+                   aria-label="a dense table"
+                   sx={{width: '100%'}}
+            >
+                <TableHead>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Actions</TableCell>
+                </TableHead>
+
+                {
+                    props.result.map((item, index) => {
+                        return (
+                            <TableRow
+                                key={index}
+                                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                            >
+                                <TableCell key={index}>{item.user_email}</TableCell>
+                                <TableCell key={index}>{item.actions_cnt}</TableCell>
+                            </TableRow>
+                        )
+                    })
+                }
+            </Table>
+        </TableContainer>
     )
 }
 
